eaa522136282279aa1b2f5780b6eeeaf5a37ea5a
Tyler Wiersing <tyler.wiersing@gmail.com>
2018-10-11 10:05:51 -0400

    fix naming on view vars

--- auklet/middleware.py
+++ auklet/middleware.py
@@ -19,10 +19,10 @@ class AukletMiddleware(MiddlewareMixin):
         client = get_client()
         client.produce_event(exc_type, traceback)
 
-    def process_view(self, request, view_func, callback_args, callback_kwargs):
+    def process_view(self, request, view_func, view_args, view_kwargs):
         client = get_client(monitor=True)
         client.monitoring.start()
-        response = view_func(request, *callback_args, **callback_kwargs)
+        response = view_func(request, *view_args, **view_kwargs)
         client.monitoring.stop()
         client.produce_stack(client.monitoring.get_stack())
         return response
